2d5269ad6a6ac69634382da4ddd59f0970b8623a
Willyam Ramos <willyamramosdutra@gmail.com>
2023-11-23 09:40:47 -0300

    Criado funcionalidade para home admin, botões agora abrem o intent do ListCourses e AdminActivity

--- app/src/main/java/com/ritter/cursoextensao/ui/fragments/HomeAdminFragment.java
+++ app/src/main/java/com/ritter/cursoextensao/ui/fragments/HomeAdminFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -13,16 +14,21 @@ import android.widget.EditText;
 import com.ritter.cursoextensao.AdminActivity;
 import com.ritter.cursoextensao.ListCourse;
 import com.ritter.cursoextensao.R;
+import com.ritter.cursoextensao.data.DataBaseHelper;
+
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class HomeAdminFragment extends Fragment {
 
     Button btn_save;
     Button btn_list;
 
-
+    private DataBaseHelper dbHelper;
+    private TextView counterAlunos;
+    private TextView counterCursos;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,7 +41,12 @@ public class HomeAdminFragment extends Fragment {
         // Inicialize elementos de UI e lógica específica para o ALUNO aqui
         btn_save = view.findViewById(R.id.btn_save_curso);
         btn_list = view.findViewById(R.id.btn_list_curso);
+        dbHelper = new DataBaseHelper(getActivity());
 
+        counterAlunos = view.findViewById(R.id.counterAlunos);
+        counterCursos = view.findViewById(R.id.counterCursos);
+
+        updateCounters();
 
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,5 +71,12 @@ public class HomeAdminFragment extends Fragment {
         //}
 
         return view;
+    }
+    private void updateCounters(){
+        int alunosCount = dbHelper.countAllStudents();
+        int cursosCount = dbHelper.countAllCourses();
+
+        counterAlunos.setText(String.valueOf(alunosCount));
+        counterCursos.setText(String.valueOf(cursosCount));
     }
 }
